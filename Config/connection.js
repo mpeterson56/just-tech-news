1d9d86d7de911f76f65100d4790e8a436997db8b
Mike Peterson <mikemikemike3390@yahoo.com>
2022-06-09 18:17:08 -0700

    on to the next one

--- Config/connection.js
+++ Config/connection.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: 'localhost',
   dialect: 'mysql',
-  port: 3306
+  port: 3001
 });
 
 module.exports = sequelize;
